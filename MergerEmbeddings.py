b8145eabfa1bb7a72bd671dde7763f3b7c1ce474
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-03-21 17:25:30 +0300

    Fix file names

--- MergerEmbeddings.py
+++ MergerEmbeddings.py
@@ -6,13 +6,13 @@ frames = []
 not_founds = []
 for idx in range(0, 14000, 100):
     print(f"\r{round((idx/14000), 2)*100}%", end="")
-    if os.path.isfile(f'./data/sep/embeddings_{idx}_{idx+100}.json'):
-        tmp = pd.read_json(f'./data/sep/embeddings_{idx}_{idx+100}.json')
+    if os.path.isfile(f'./data/sep/embeddings_labeled_{idx}_{idx+100}.json'):
+        tmp = pd.read_json(f'./data/sep/embeddings_labeled_{idx}_{idx+100}.json')
         tmp = tmp[['form', 'year', 'embeddings', 'label']].copy()
         tmp['embeddings'] = tmp['embeddings'].apply(lambda x: list(np.array(x).astype(np.float16)))
         frames.append(tmp)
     else:
-        not_founds.append(f"embeddings_{idx}_{idx+100}.json")
+        not_founds.append(f"embeddings_labeled_{idx}_{idx+100}.json")
 
 
 data = pd.concat(frames, ignore_index=True)
