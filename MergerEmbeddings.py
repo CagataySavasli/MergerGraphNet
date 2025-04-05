a3bee7c828ac8b6a3f0be20c13b7467b8c41b5a7
Ahmet Çağatay Savaşlı <cagatay.savasli@ozu.edu.tr>
2025-04-05 18:52:02 +0300

    Convert memory efficient way

--- MergerEmbeddings.py
+++ MergerEmbeddings.py
@@ -1,45 +1,35 @@
 import os
 import pandas as pd
-import numpy as np
 
 # Çıktı dosya yolları
-train_csv = 'data/processed/embedded_labeled.csv'
+train_csv = 'data/processed/embedded_labeled.parquet'
 
 # Varolan çıktı dosyalarını temizle (varsa)
 if os.path.exists(train_csv):
     os.remove(train_csv)
 
 not_founds = []
-
+founds = []
 print("Start merging embeddings in memory efficient way")
 # Dosyalar 0'dan 14000'e kadar her 100 adımında işlenecek
-for idx in range(0, 14000, 100):
-    file_path = f'./data/sep/embeddings_labeled_{idx}_{idx + 100}.json'
+for idx in range(0, 13100, 100):
+    file_path = f'./data/sep/embeddings_labeled_{idx}_{idx + 100}.parquet'
 
     if os.path.isfile(file_path):
         try:
-            # JSON dosyasını oku
-            tmp = pd.read_json(file_path)
-
-            # Dosyada yeterli veri varsa işle
-            if len(tmp) >= 5:
-                # İlgili sütunları seç ve embeddings'i float16'ya çevir
-                tmp = tmp[['form', 'year', 'embeddings', 'label']].copy()
-                tmp['embeddings'] = tmp['embeddings'].apply(lambda x: list(np.array(x).astype(np.float16)))
-
-
-                # Verileri CSV'ye ekle (ilk seferde header yaz, sonraki seferde ekle)
-                if not tmp.empty:
-                    if os.path.exists(train_csv):
-                        tmp.to_csv(train_csv, mode='a', header=False, index=False)
-                    else:
-                        tmp.to_csv(train_csv, mode='w', header=True, index=False)
-
+            tmp = pd.read_parquet(file_path)
+            founds.append(tmp)
         except Exception as e:
             print(f"Error processing file {file_path}: {e}")
     else:
         not_founds.append(file_path)
     print(f"Processed file: {file_path}")
+
+# Birleştirilmiş DataFrame'i oluştur
+if founds:
+    merged_df = pd.concat(founds, ignore_index=True)
+    merged_df.to_parquet(train_csv, index=False)
+    print(f"Merged DataFrame saved to {train_csv}")
 
 print("## Not Found Files:")
 for not_found in not_founds:
